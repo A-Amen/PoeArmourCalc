659ba7b9992b967892e6b87dc87b42fa20b1718a
unknown <adityasureshmenon@gmail.com>
2020-06-07 21:25:42 +0530

    initial design. buttons still not functional

---
+++ function.js
@@ -0,0 +1,12 @@
+
+function healthfromhit(){
+    // document.getElementById('toFill').innerHTML = "in health from hit"
+    alert("in health dmg hit")
+}
+
+function dmgfromhit(){
+    // document.getElementById('toFill').innerHTML = "in damage from hit"
+    alert("in dmg hit")
+}
+
+document.getElementById("calcHealthFromHit").addEventListener("click", healthfromhit());
